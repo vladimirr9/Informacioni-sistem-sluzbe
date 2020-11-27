ee870d089e62023d34bfe077b97600316b5bb94e
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2020-11-27 18:44:15 +0100

    #glavni_prozor

---
+++ src/rs/ac/uns/MainFrame.java
@@ -0,0 +1,26 @@
+package rs.ac.uns;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame{
+	public MainFrame() {
+		super();
+		Toolkit tk=Toolkit.getDefaultToolkit();
+		Dimension dim=tk.getScreenSize();
+		setSize(3*dim.width/4,3*dim.height/4);
+		setTitle("Studentska Služba");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(true);
+		
+		JPanel panel1=new JPanel();
+		JLabel labela1=new JLabel("TODO: Prikaz entiteta sistema");
+		panel1.add(labela1);
+		this.add(panel1);
+	}
+}
---
+++ src/rs/ac/uns/MyApp.java
@@ -0,0 +1,11 @@
+package rs.ac.uns;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		MainFrame mf=new MainFrame();
+		mf.setVisible(true);
+
+	}
+
+}
