1423467fc4c95417ddf944d716b2f97f4c189869
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2020-12-24 23:30:48 +0100

    #glavni_prozor dodati tabovi

---
+++ src/rs/ac/uns/JTabbedPaneMine.java
@@ -0,0 +1,11 @@
+package rs.ac.uns;
+
+import javax.swing.JTabbedPane;
+
+public class JTabbedPaneMine extends JTabbedPane {
+	
+	public JTabbedPaneMine() {
+		super();
+	}
+	
+}
--- src/rs/ac/uns/MainFrame.java
+++ src/rs/ac/uns/MainFrame.java
@@ -14,7 +14,7 @@ public class MainFrame extends JFrame{
 		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension dim=tk.getScreenSize();
 		setSize(3*dim.width/4,3*dim.height/4);
-		setTitle("Studentska Sluûba");
+		setTitle("Studentska Slu≈æba");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(true);
@@ -22,12 +22,24 @@ public class MainFrame extends JFrame{
 		JPanel panel1=new JPanel();
 		JLabel labela1=new JLabel("TODO: Prikaz entiteta sistema");
 		panel1.add(labela1);
-		this.add(panel1);
+		JPanel panel2=new JPanel();
+		JLabel labela2=new JLabel("TODO: Prikaz entiteta sistema");
+		panel2.add(labela2);
+		JPanel panel3=new JPanel();
+		JLabel labela3=new JLabel("TODO: Prikaz entiteta sistema");
+		panel3.add(labela3);
+		
 		MyMenuBar myMenu = new MyMenuBar(this);
 		this.setJMenuBar(myMenu);
 		StatusBar sBar = new StatusBar();
 		this.add(sBar, BorderLayout.SOUTH);
 		MyToolbar toolbar=new MyToolbar();
 		this.add(toolbar,BorderLayout.NORTH);
+		
+		JTabbedPaneMine tabbedPane=new JTabbedPaneMine();
+		tabbedPane.addTab("Studenti", panel1);
+		tabbedPane.addTab("Profesori", panel2);
+		tabbedPane.addTab("Predmeti", panel3);
+		this.add(tabbedPane,BorderLayout.CENTER);
 	}
 }
