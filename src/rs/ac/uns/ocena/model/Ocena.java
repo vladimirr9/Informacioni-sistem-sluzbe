b23cf7689b659ad9ba5d831630accb57c86ab06f
Vladimir <rokvic.ra69.2018@uns.ac.rs>
2020-12-26 22:23:44 +0100

    #ocena Implementiran model za ocenu

--- src/rs/ac/uns/ocena/model/Ocena.java
+++ src/rs/ac/uns/ocena/model/Ocena.java
@@ -2,12 +2,58 @@ package rs.ac.uns.ocena.model;
 
 import java.util.Date;
 
+import rs.ac.uns.predmet.model.Predmet;
 import rs.ac.uns.student.model.Student;
 
 public class Ocena {
 
 	private Student student;
-	//TODO: private Predmet predmet; 
+	Predmet predmet; 
 	private int ocena;
 	private Date datumPolaganja;
+	
+	
+	
+	
+	
+	
+	
+	public Ocena() {
+		super();
+	}
+	public Ocena(Student student, Predmet predmet, int ocena, Date datumPolaganja) {
+		super();
+		this.student = student;
+		this.predmet = predmet;
+		this.ocena = ocena;
+		this.datumPolaganja = datumPolaganja;
+		
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Predmet getPredmet() {
+		return predmet;
+	}
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+	public int getOcena() {
+		return ocena;
+	}
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+	public Date getDatumPolaganja() {
+		return datumPolaganja;
+	}
+	public void setDatumPolaganja(Date datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+	
+	
+	
 }
