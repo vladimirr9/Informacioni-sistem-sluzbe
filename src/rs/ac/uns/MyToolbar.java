ac74e735eb99763c118927a921c267f99a1983b5
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2021-01-13 04:38:17 +0100

    #pretraga_profesora

--- src/rs/ac/uns/MyToolbar.java
+++ src/rs/ac/uns/MyToolbar.java
@@ -6,7 +6,11 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.Action;
 import javax.swing.ImageIcon;
@@ -18,12 +22,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
+import javax.swing.RowFilter;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.table.TableRowSorter;
 
 import rs.ac.uns.abstractActions.DeleteAction;
 import rs.ac.uns.abstractActions.EditAction;
 import rs.ac.uns.abstractActions.NewAction;
+import rs.ac.uns.profesor.view.AbstractTableModelProfesori;
 
 public class MyToolbar extends JToolBar {
 	public MyToolbar(final JFrame parent) {
@@ -83,7 +90,30 @@ public class MyToolbar extends JToolBar {
 		btn4.setBorderPainted(false);
 		gbc=new GridBagConstraints(5, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		this.add(btn4,gbc);
-	
+		
+		btn4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(MainFrame.tabbedPane.getSelectedIndex()==1) {
+					String unesenTekst=txtField.getText();
+					String delovi[]=unesenTekst.split(" ");
+					AbstractTableModelProfesori model = (AbstractTableModelProfesori) MainFrame.getInstance().getTabelaProfesora().getModel();
+					final TableRowSorter< AbstractTableModelProfesori> sorter = new TableRowSorter< AbstractTableModelProfesori>(model);
+					MainFrame.getInstance().getTabelaProfesora().setRowSorter(sorter);
+					if(delovi.length==1) {
+					sorter.setRowFilter(RowFilter.regexFilter("^(?i)" + unesenTekst,1));
+					} else if(delovi.length==2) {
+						 List<RowFilter<Object, Object>> filteri = new ArrayList<RowFilter<Object, Object>>(2);
+			                filteri.add(RowFilter.regexFilter("(?i)" + delovi[0], 1));
+			                filteri.add(RowFilter.regexFilter("(?i)" + delovi[1], 0));
+			                RowFilter filter=RowFilter.andFilter(filteri);
+			                sorter.setRowFilter(filter);
+					}
+				}
+				
+			}
+		});
 		
 	}
 }
